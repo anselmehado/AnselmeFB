0221032286bb7f4036f60b80a0690ccecd9f4192
hado anselme <anselmehado85@yahoo.com>
2021-05-13 17:13:37 +0100

    ok

--- app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ApplicationRecord
-  validates :content, presence: true, length: {maximum: 140}
-  belongs_to :user
-  has_many :favorites, dependent: :destroy
-  has_many :favorite_users, through: :favorites, source: :user
-
-  mount_uploader :avatar, ImageUploader
-end
